da364805c705ad49967aae7e851f93cf70fda7f8
Marylange <marylangess@gmail.com>
2020-02-18 18:38:45 -0300

    segunda questao

--- exercise_2.js
+++ exercise_2.js
@@ -1,6 +1,8 @@
-let objeto = {
-    personagem: 'Margarida',
-    origem: 'Pato Donald',
-    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+let info = {
+    personagem: "Margarida",
+    origem: "Pato Donald",
+    nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
     
 }
+info.recorrente = "sim";
+console.log(info);
