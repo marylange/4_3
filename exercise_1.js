b99e028d06e9a31ca65a7da8f085a4607b96dd83
Marylange <marylangess@gmail.com>
2020-02-18 18:32:31 -0300

    atualizando os exercicios

---
+++ exercise_1.js
@@ -0,0 +1,9 @@
+let info = {
+    personagem: "Margarida",
+    origem: "Pato Donald",
+    nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+};
+
+for(let indice in info){
+    console.log("Bem Vinda,",info.personagem);
+}
---
+++ exercise_2.js
@@ -0,0 +1,6 @@
+let objeto = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    
+}
